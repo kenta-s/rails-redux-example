c3dc88e663c15747888365f48654fedc4cf0c9db
Kenta <shirai.kenta@vareal.co.jp>
2016-05-28 10:16:56 +0900

    Create todos api

--- server/todos/config/routes.rb
+++ server/todos/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :todos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, { format: 'json' } do
+    namespace :v1 do
+      resources :todos
+    end
+  end
 end
